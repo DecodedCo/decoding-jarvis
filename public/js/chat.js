7cb549c9c5539a9f1e9e0861180ee2375665138a
arianadziedzic <ariana.dziedzic@gmail.com>
2018-05-10 15:34:00 -0400

    add socket connection for new commands

---
+++ public/js/chat.js
@@ -0,0 +1,41 @@
+(function () {
+
+  // makes connection to server socket
+  var socket = io.connect('http://localhost:3000');
+
+  // connects the html value of buttons & inputs
+  var message = document.getElementById("message");
+  var send_message = document.getElementById("send_message");
+  var chatroom = document.getElementById('chatroom');
+
+  // Emit message
+  send_message.onclick = function() {
+    socket.emit('new_message', {
+      message: message.value
+    });
+    // clears message input after intent is submitted
+    message.value = '';
+  }
+
+  //submit message on enter key press as well
+  document.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+
+      socket.emit('new_message', {
+        message: message.value
+      });
+      // clears message input after intent is submitted
+      message.value = '';
+    }
+  })
+
+  // Listen to new message & renders new intent as a paragrap
+  socket.on("new_message", (data) => {
+    // when there's a new message, create new paragraph & post it
+    updated_message = document.createElement("p");
+    updated_message.innerHTML += data.message;
+    chatroom.insertBefore(updated_message, chatroom.childNodes[0])
+  })
+
+})();
