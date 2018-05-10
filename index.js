471fa4d34a41c8184480a487a8a7b998cfc9e372
arianadziedzic <ariana.dziedzic@gmail.com>
2018-05-10 14:33:26 -0400

    add express & socket.io connection

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
-// ************************ INSTANTIATE EXPRESS ************************
+// *********** INSTANTIATE EXPRESS *************
 const express = require('express');
 const app = express();
+const http = require('http').Server(app);
 
-// ***************** ACCESS TO HTML, CSS, JS & IMAGES *******************
+// ***************** ROUTES *******************
 app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/public'));
-
-
-// ************************ ROUTES **********************
 
 app.get('/', (req, res) => {
   res.sendFile('index.html');
 });
 
-// ************************ STARTS SERVER ************************
-const server = app.listen(3000);
+http.listen(3000);
+
+// *************** SOCKET.IO *****************
+
+const io = require('socket.io')(http);
+
+io.on('connection', (socket) => {
+  console.log('a user is connected');
+
+  socket.on('new_message', (data) => {
+    io.sockets.emit('new_message', {
+      message: data.message
+    });
+  });
+
+});
